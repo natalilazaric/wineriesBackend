51413b09e62e6eeae4f26d030e62d6347403f9b2
natalilazaric <nl53743@fer.hr>
2025-04-20 17:17:37 +0200

    Dodan CorsConfig

--- src/main/java/com/appwineries/Wineries/security/CorsConfig.java
+++ src/main/java/com/appwineries/Wineries/security/CorsConfig.java
@@ -14,8 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedOrigins("https://wineries-frontend-deploy.vercel.app/",  "http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
